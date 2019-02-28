0cfeed75d193120a7456b24f302fb32e1fe64ad8
Durwasa Chakraborty <durwasa@Durwasas-MacBook-Pro.local>
2019-02-28 15:43:20 -0500

    Fixed linting error : test_models.py

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -18,7 +18,7 @@ class ModelTests(TestCase):
     def test_new_user_email_normalized(self):
         """Test the email for a new user is normalized"""
         email = 'test@GMAIL.COM'
-        user  = get_user_model().objects.create_user(email, 'test123')
+        user = get_user_model().objects.create_user(email, 'test123')
         self.assertEqual(user.email, email.lower())
 
     def test_new_user_invalid_email(self):
@@ -28,6 +28,9 @@ class ModelTests(TestCase):
 
     def test_create_new_superuser(self):
         """Test creating a new superuser"""
-        user = get_user_model().objects.create_superuser('test@gmail.com', 'Test123')
+        user = get_user_model().objects.create_superuser(
+            'test@gmail.com',
+            'Test123'
+        )
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
