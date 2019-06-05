f370710406e5b69d939f67140ffe825c2a2fbb15
Durwasa Chakraborty <durwasa@Durwasas-MacBook-Pro.local>
2019-06-05 12:39:53 -0400

    updated admin to support custom module

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,39 @@
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+class AdminSiteTests(TestCase):
+    def setUp(self):
+        """ setup function """
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@gmail.com',
+            password='password123'
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='admin123@gmail.com',
+            password='password123',
+            name='Test user Full Name'
+        )
+
+    def test_users_listed(self):
+        """Test that users are listed on user page"""
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
+
+    def test_user_change_page(self):
+        """test that change edit page work"""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """test that add user page works"""
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
